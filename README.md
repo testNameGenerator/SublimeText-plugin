fc8d8131989c28ed2b45caf1f4ef57c8ca5b4c87
Bogdan Anton <contact@bogdananton.ro>
2014-07-22 23:28:17 +0300

    added support for Jasmine. uses syntax to choose between test syntax

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 testNameGenerator
 ==============
 
-Will convert plain text task/message into a test method, using the plain text as a comment for readability
+Will convert plain text task/message into a test method, using the plain text as a comment for readability.
+
+Uses the file's syntax to choose between PHPUnit syntax (for PHP files) or Jasmine's "describe" and "it" blocks (for JavaScript).
+If the syntax is not PHP or JavaScript, no updates will be applied to the file.
+
 
 Usage
 -----
@@ -9,12 +13,18 @@ Usage
    * Place cursor in the line(s) containing the test name(s)
    * Press `Ctrl+Shift+u` to convert the line(s) into test(s)
 
+For JavaScript, the default action will be to create a Jasmine "it" block. Add "describe " as a prefix to the string to create a "describe block".
+
 Limitations
 -----
-Only supports PHPUnit syntax, for now.
+Only supports PHPUnit syntax and Jasmine (describe and it blocks), for now.
 
 TODO: context menu for selecting different languages / syntaxes
 
 Preview
 -----
+PHP
 ![](https://raw.githubusercontent.com/bogdananton/Sublime-testNameGenerator/master/preview.png)
+
+JavaScript
+![](https://raw.githubusercontent.com/bogdananton/Sublime-testNameGenerator/master/preview-jasmine.png)
--- TestNameGenerator.py
+++ TestNameGenerator.py
@@ -6,29 +6,45 @@ class ConvertTestNameCommand(sublime_plugin.TextCommand):
         # get a list of [test name with separate words, test name with no spaces in between and camel cased] from the current cursor(s) or selection(s)
         extractedTestNames = []
 
+        currentSyntax = self.getSyntax()
+        if ((currentSyntax != "PHP") & (currentSyntax != "JavaScript")):
+            return
+
         # iterate over all cursors
         for region in self.view.sel():
             # extract line contents from cursor(s)
             line = self.view.line(region)
             lineContents = self.view.substr(line).strip()
 
-            # clear non-alpha and non-space chars
-            testNameExpanded = re.sub(r'[^a-zA-Z0-9 \(\)]', '', lineContents).strip()
+            # keep only alphanum and ",", ".", "(", ")"
+            testNameExpanded = re.sub(r'[^a-zA-Z0-9., \(\)]', '', lineContents).strip()
 
             # if extracted line contents are not empty, add them to stack
             if (testNameExpanded):
-                # convert to CamelCase
-                testNameContracted = testNameExpanded.title().replace(" ", "").replace("(", "").replace(")", "")
+                # convert to CamelCase and keep only alphanumeric chars
+                testNameContracted = testNameExpanded.title()
+                testNameContracted = re.sub(r'[^a-zA-Z0-9]', '', testNameContracted)
 
                 # replace current line
-                self.view.replace(edit, line, self.prepareTestContent(testNameExpanded, testNameContracted))
+                self.view.replace(edit, line, self.prepareTestContent(testNameExpanded, testNameContracted, currentSyntax))
 
-    def prepareTestContent(self, testNameExpanded, testNameContracted):
-        return """    /**
-     * """+testNameExpanded+"""
-     */
-    public function test"""+testNameContracted+"""()
-    {
+    def prepareTestContent(self, testNameExpanded, testNameContracted, languageSyntax):
+        returnContent = ""
 
-    }
-    """
+        if (languageSyntax == "PHP"):
+            returnContent = "    /**\n     * "+testNameExpanded+"\n     */\n    public function test"+testNameContracted+"()\n    {\n\n    }\n    "
+
+        elif (languageSyntax == "JavaScript"):
+            # will generate Jasmine blocks
+            examineNameTypeParts = testNameExpanded.split(" ")
+
+            if (examineNameTypeParts[0].lower() == "describe"):
+                examineNameTypeParts.pop(0) # remove the "describe" prefix
+                returnContent = "    describe('"+ (" ".join(examineNameTypeParts)) +"', function () {\n\n    });\n    "
+            else:
+                returnContent = "        it('"+testNameExpanded+"', function () {\n\n        });\n    "
+
+        return returnContent
+
+    def getSyntax(self):
+        return self.view.settings().get('syntax').replace('.tmLanguage', '').split("/")[1]
